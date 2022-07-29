cca10d70b819388a2bd33c3e26b7a6e2e643389f
gianfra097 <giandesign1997@gmail.com>
2022-07-29 20:02:00 +0200

    Non legge file kv

---
+++ main.py
@@ -0,0 +1,11 @@
+import kivy
+from kivy.app import App
+
+class DiabeticDiaryApp(App):
+    pass
+    #Creo la finestra iniziale dell'app
+    #def build(self):                   
+    #    self.title = "DiabeticDiary"
+    #    self.theme_cls.theme_style = "Light"
+
+DiabeticDiaryApp().run()
