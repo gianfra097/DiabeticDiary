f6b8d0ac688410f180a4502934432f1e6345a6df
gianfra097 <82977399+gianfra097@users.noreply.github.com>
2022-08-04 16:43:55 +0200

    Aggiornamento lezione 03/08/2022

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+from ast import NotIn
+from kivy.utils import _get_platform
+from kivy.config import Config  #Documentazione
+if _get_platform() not in ["android", "ios"]:
+    Config.set("graphics","width",368)
+    Config.set("graphics","height",712)
 import kivy
 from kivy.app import App
 
 class DiabeticDiaryApp(App):
     pass
-    #Creo la finestra iniziale dell'app
-    #def build(self):                   
-    #    self.title = "DiabeticDiary"
-    #    self.theme_cls.theme_style = "Light"
 
 DiabeticDiaryApp().run()
